de06ad9e7492d0970d17ae0d6d1bcfeb29f4ac23
Camille Rubia <va.camillerubia@gmail.com>
2023-04-21 09:53:33 +0800

    Sandbox testing and C# Prep 1 Assignment

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!!");
+        Console.Write("Testing Write");
+        Console.WriteLine("Testing WriteLine.");
+        Console.WriteLine("What is your name?");
+        string user_input_name = Console.ReadLine();
+
+        Console.WriteLine($"Nice to meet you, {user_input_name}!");
+        
     }
 }
