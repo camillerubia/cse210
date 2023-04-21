e6c137415fa55c9dea46d6fffcef77c20c5ae97f
Camille Rubia <va.camillerubia@gmail.com>
2023-04-21 15:38:10 +0800

    Prep 2 - Conditionals

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,73 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Prep 2 - Conditionals");
+        Console.WriteLine();
+
+        Console.Write("What is your grade percentage? ");
+        int gradePercentage = int.Parse(Console.ReadLine());
+
+        // Type inputType = gradePercentage.GetType();
+        // Console.WriteLine(inputType);
+
+        string gradeEquivalent = null;
+
+            if (gradePercentage >= 90)
+            {
+                gradeEquivalent = "A";
+            }
+            else if (gradePercentage >= 80)
+            {
+                gradeEquivalent = "B";
+            }
+            else if (gradePercentage >= 70)
+            {
+                gradeEquivalent = "C";
+            }
+            else if (gradePercentage >= 60)
+            {
+                gradeEquivalent = "D";
+            }
+            else if (gradePercentage < 60)
+            {
+                gradeEquivalent = "F";
+            }
+
+        int lastNumber = gradePercentage % 10;
+        string plus = "+";
+        string minus = "-";
+
+            if (lastNumber >= 7)
+            {
+                gradeEquivalent += plus;
+            }
+                if (gradeEquivalent == "A+" || gradeEquivalent == "F+")
+                {
+                    gradeEquivalent = gradeEquivalent.Replace(plus, "");
+                }  
+            else if (lastNumber < 3)
+            {
+                gradeEquivalent += minus;
+            }
+                if (gradeEquivalent == "F-")
+                {
+                    gradeEquivalent = gradeEquivalent.Replace(minus, "");
+                }
+
+            
+
+
+        Console.WriteLine($"Grade Equivalent: {gradeEquivalent}");
+
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed!");
+            Console.WriteLine();
+        }
+        else
+        {
+            Console.WriteLine("Sorry, please try again.");
+            Console.WriteLine();
+        }
     }
 }
