ecde2073749dfa253537992aa5a025587fa3588e
Camille Rubia <va.camillerubia@gmail.com>
2023-04-24 14:53:17 +0800

    Created randomizer

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Prep 3 - Loops");
+
+        Random randomizer = new Random();
+        int randomNumber = randomizer.Next(1, 11);
+
+        Console.WriteLine($"What is the magic number? {randomNumber}");
     }
 }
