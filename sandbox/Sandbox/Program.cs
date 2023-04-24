9e328b0ab443d0158592a1458ce8a607ab6004b2
Camille Rubia <va.camillerubia@gmail.com>
2023-04-24 12:04:54 +0800

    Testing foreach loop in sandbox

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,14 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!!");
-        Console.Write("Testing Write");
-        Console.WriteLine("Testing WriteLine.");
-        Console.WriteLine("What is your name?");
-        string userName = Console.ReadLine();
+        // Console.WriteLine("Hello Sandbox World!!");
+        // Console.Write("Testing Write");
+        // Console.WriteLine("Testing WriteLine.");
+        // Console.WriteLine("What is your name?");
+        // string userName = Console.ReadLine();
 
-        Console.WriteLine($"Nice to meet you, {userName}!");
+        // Console.WriteLine($"Nice to meet you, {userName}!");
         
-        Console.WriteLine(userName.GetType());
+        // Console.WriteLine(userName.GetType());
+
+        int [] numbers = { 1, 2, 3 };
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
     }
 }
