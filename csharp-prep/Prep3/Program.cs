18c4a25b2c02df41986cf3b942b867c0ab80c754
Camille Rubia <va.camillerubia@gmail.com>
2023-04-24 15:14:30 +0800

    Created if else conditions

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -9,10 +9,23 @@ class Program
         Random randomizer = new Random();
         int randomNumber = randomizer.Next(1, 11);
 
+        Console.WriteLine();
         Console.WriteLine($"What is the magic number? {randomNumber}");
         Console.Write("What is your guess? ");
         int userGuess = int.Parse(Console.ReadLine());
 
-
+        
+            if (userGuess < randomNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (userGuess > randomNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (userGuess == randomNumber)
+            {
+                Console.WriteLine("You guessed it!");
+            }
     }
 }
