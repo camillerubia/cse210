6f695c46ff26fa7c50b8a02be527163e5e7f32b5
Camille Rubia <va.camillerubia@gmail.com>
2023-04-24 16:22:07 +0800

    Testing the overall while loop to play again

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,38 +5,46 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Prep 3 - Loops");
-        Console.WriteLine("Guess the magic number from 1-100.");
 
-        Random randomizer = new Random();
-        int randomNumber = randomizer.Next(1, 101);
+        string playAgain = "";
 
-        Console.WriteLine();
-        Console.WriteLine($"What is the magic number? {randomNumber}");
-        
-       int userGuess = 0;
-       int guessCounter = 1;
-
-        while (userGuess != randomNumber)
+        while (playAgain == "yes")
         {
             Console.WriteLine();
-            Console.WriteLine($"Guess #{guessCounter}");
-            Console.Write("What is your guess? ");
-            userGuess = int.Parse(Console.ReadLine());
-            guessCounter ++;
+            Console.WriteLine("Guess the magic number from 1-100.");
 
-            if (userGuess < randomNumber)
-            {
-                Console.WriteLine("Higher");
+            Random randomizer = new Random();
+            int randomNumber = randomizer.Next(1, 101);
 
-            }
-            else if (userGuess > randomNumber)
+            Console.WriteLine();
+            Console.WriteLine($"What is the magic number? {randomNumber}");
+            
+            int userGuess = 0;
+            int guessCounter = 1;
+
+            while (userGuess != randomNumber)
             {
-                Console.WriteLine("Lower");
-            }
-            else if (userGuess == randomNumber)
-            {
-                Console.WriteLine("You guessed it!");
+                Console.WriteLine();
+                Console.WriteLine($"Guess #{guessCounter}");
+                Console.Write("What is your guess? ");
+                userGuess = int.Parse(Console.ReadLine());
+                guessCounter ++;
+
+                if (userGuess < randomNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (userGuess > randomNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (userGuess == randomNumber)
+                {
+                    Console.WriteLine("You guessed it!");
+                }
             }
         }
+            Console.Write("Do you want to play again? (yes or no): ");
+            playAgain = Console.ReadLine();
     }
 }
