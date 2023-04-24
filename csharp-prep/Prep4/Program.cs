6bec77822a495f7646124d0065f1f7edc40710a9
Camille Rubia <va.camillerubia@gmail.com>
2023-04-24 20:54:18 +0800

    Created a list, and tested while loop.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,24 @@
-using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Prep 4 - Lists");
+        Console.WriteLine();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished");
+
+        int userNumber = 1;
+        List<int> numbers = new List<int> ();
+
+        while (userNumber != 0)
+        {
+            Console.Write("Enter number: ");
+            userNumber = int.Parse(Console.ReadLine());
+            numbers.Add(userNumber);
+        }
+
+        Console.WriteLine(numbers);
     }
 }
