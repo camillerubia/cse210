97af57ef2dda07f3877dc35355b46837cc52b2fa
Camille Rubia <va.camillerubia@gmail.com>
2023-04-25 15:41:09 +0800

    Tested the program and wrote documentations.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -10,36 +11,52 @@ class Program
 
         Console.WriteLine("Enter a list of numbers, type 0 when finished");
 
+        // Set an initial value for userNumber as an identifier in the while loop.
         int userNumber = 1;
+
+        // Creates an int list for storing user input numbers.
         List<int> numbers = new List<int> ();
 
+        // Loops the program until user inputs 0.
         while (userNumber != 0)
         {
             Console.Write("Enter number: ");
+
+            // Converts the user input from string to int.
             userNumber = int.Parse(Console.ReadLine());
 
+            // Stores user input numbers that are not 0.
             if (userNumber != 0)
             {
                 numbers.Add(userNumber);
             }
         }
 
+        // Stores the sum, average, max and min numbers in a variable from the created list.
         int sum = numbers.Sum();
         double average = numbers.Average();
         int maxNumber = numbers.Max();
+
+        // Identifies and returns positive numbers from the list using Lambda function
+        // that separates it from the negative numbers and stores it in a new list.
         List<int> posNum = numbers.Where(n => n > 0).ToList();
         int minPosNumber = posNum.Min();
 
-        Console.WriteLine();
-        Console.WriteLine($"The sum is {sum}");
-        Console.WriteLine($"The average is {average}");
-        Console.WriteLine($"The largest number is {maxNumber}");
-        Console.WriteLine();
-
-        Console.WriteLine($"The smallest positive number is: {minPosNumber}");
+        // Sorts the created number list.
         numbers.Sort();
 
+        // Prints the results. - Core Requirements
+        Console.WriteLine();
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {maxNumber}");
+        Console.WriteLine();
+
+        // Prints Stretch results.
+        Console.WriteLine($"The smallest positive number is: {minPosNumber}");
         Console.WriteLine("The sorted list is:");
+
+        // Iterates and prints the sorted list.
         foreach (int i in numbers)
         {
             Console.WriteLine(i);
