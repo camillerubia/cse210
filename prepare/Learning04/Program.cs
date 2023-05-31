46795b06c73868b60bddf62a9bd1f04702b4f3a1
Camille Rubia <va.camillerubia@gmail.com>
2023-05-31 21:58:52 +0800

    Finished the WritingAssignment Class

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -7,11 +7,15 @@ class Program
         Console.WriteLine("Hello Learning04 World!");
         Console.WriteLine("Inheritance");
 
-        // Assignment assignment = new Assignment("Nica Rubia", "C#");
-        // Console.WriteLine(assignment.GetSummary());
+        Assignment assignment = new Assignment("Nica Rubia", "C#");
+        Console.WriteLine(assignment.GetSummary());
 
         MathAssignment mathAssign = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
         Console.WriteLine(mathAssign.GetSummary());
         Console.WriteLine(mathAssign.GetHomeworkList());
-    }
+
+        WritingAssignment writeAssign = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(writeAssign.GetSummary());
+        Console.WriteLine(writeAssign.GetWritingInformation());
+    }   
 }
