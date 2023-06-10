55cc6d31fd8e8614b9bf0b75aef2cf1ebabc6732
Camille Rubia <va.camillerubia@gmail.com>
2023-06-10 20:25:42 +0800

    Worked on saving and loading from a file, which includes the date and the Activity log along with a new method

--- prove/Develop04/LogManager.cs
+++ prove/Develop04/LogManager.cs
@@ -1,30 +1,52 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
 
 public class LogManager
 {
     public List<string> _logOptions = new List<string> {"Display", "Save", "Load"};
-    private string[] _logSummary = new List<string>();
+    private List<string> _fileLogSummary = new List<string>();
+    private List<string> _logSummary = new List<string>();
     public int _breathingCounter;
     public int _reflectingCounter;
     public int _listingCounter;
     private int _userChoice;
     private string _filename;
 
+    // Declares the Datetime and stores it into a static field.
+    static DateTime dateNow = DateTime.Now;
+    // Stores the current date into a local variable.
+    string _currentDate = dateNow.ToShortDateString();
+    // Declares a static field with the current time and converted it to uppercase.
+    static string _currentTime = dateNow.ToString("h:mm tt").ToUpper();
 
     public void DisplayLogOptions()
     {
-        Console.Clear();
-        Console.WriteLine("Summary Log Options:");
-
-        for (int i = 0; i < _logOptions.Count; i++)
+        while (true)
         {
-            Console.WriteLine($"{i+1}. {_logOptions[i]}");
-        }
+            Console.Clear();
+            Console.WriteLine("Summary Log Options:");
 
-        Console.Write("\nWhat do you want to do? ");
-        _userChoice = int.Parse(Console.ReadLine());
-        Console.WriteLine();
-        UserChoiceDeterminer();
+            for (int i = 0; i < _logOptions.Count; i++)
+            {
+                Console.WriteLine($"{i+1}. {_logOptions[i]}");
+            }
+
+            try
+            {
+                Console.Write("\nWhat do you want to do? ");
+                _userChoice = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+                UserChoiceDeterminer();
+                break;
+            }
+        
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid choice.\n");
+                continue;
+            }
+        }
     }
 
     private void UserChoiceDeterminer()
@@ -32,66 +54,43 @@ public class LogManager
         switch (_userChoice)
         {
             case 1:
-                DisplaySummary();
+                DisplaySummary(_logSummary);
                 break;
             case 2:
-                SaveFile();
+                SaveFile(_logSummary);
                 break;
 
             case 3:
                 LoadFile();
+                DisplaySummary(_fileLogSummary);
                 break;
         }
     }
 
-    public void DisplaySummary()
+    public void DisplaySummary(List<string> logSummary)
     {
         Console.Clear();
+        logSummary = LogEntry();
         Console.WriteLine("\nHere is your Mindfulness Summary:\n");
-        Console.WriteLine($"Breathing Activity: {_breathingCounter} times.");
-        Console.WriteLine($"Reflecting Activity: {_reflectingCounter} times.");
-        Console.WriteLine($"Listing Activity: {_listingCounter} times.\n");
+        Console.WriteLine();
+
+        foreach (string line in logSummary)
+        {
+            Console.WriteLine(line);
+        }
+
     }
 
-    public void SaveFile()
+    public void SaveFile(List<string> logSummary)
     {
         GetFileName();
-        // Declares a list to store the values from the thrown list in this method.
-        // Necessary for this method because each values are converted to be Excel-readable. 
-        List<string> logSummary = new List<string>();
-        // Assigns the headline value to a variable for an Excel heading.
-        string headline = "Date,Time,Activity,Log";
-        // Adds the headline to the created list as the first value.
-        logSummary.Add(headline);
-
-        // Iterates through the thrown list in the method.
-        foreach (string entryString in _saveList)
-        {
-            // Declares an array to store the data which has been cleaned by the Split method.
-            // Identifies what to split by using the pipe delimiter, then empty substrings are
-            // removed from the entryValues array.
-            string[] entryValues = entryString.Split(new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries);
-
-            // Stores the substrings to their specific categories by calling the ExcelWriter
-            // method then removes the whitespaces from the substring.
-            string date = ExcelWriter(entryValues[0].Trim());
-            string time = ExcelWriter(entryValues[1].Trim());
-            string prompt = ExcelWriter(entryValues[2].Trim());
-            string response = ExcelWriter(entryValues[3].Trim());
-
-            // Formats the segregated substrings into a single string and stores it in a
-            // declared variable csvEntry.
-            string csvEntry = $"{date},{time},{prompt},{response}";
-            // Adds the formatted entries into the created list from above.
-            csvEntries.Add(csvEntry);
-        }
 
         // Instantiates the SteamWriter using the outputFile variable and uses the filename 
         // field as its file title.
-        using (StreamWriter outputFile = new StreamWriter(filename))
+        using (StreamWriter outputFile = new StreamWriter(_filename))
         {
             // Writes each line from the list in the created file.
-            foreach (string line in csvEntries)
+            foreach (string line in logSummary)
             {
                 outputFile.WriteLine(line);
             }
@@ -101,7 +100,7 @@ public class LogManager
         Console.WriteLine($"\nFile saved.\n");
     }
 
-    public string[] LoadFile()
+    public List<string> LoadFile()
     {
         GetFileName();
 
@@ -109,24 +108,67 @@ public class LogManager
         {
             Console.WriteLine($"\nLoading file....");
             // Reads the file and uses the filename.
-            _logSummary = System.IO.File.ReadAllLines(_filename);
+            string[] lines = System.IO.File.ReadAllLines(_filename);
+            _fileLogSummary = lines.ToList();
+
             // A confirmation message that announces the loading of file is successful.
             Console.WriteLine($"\nFile loaded.\n");
         }
+
         catch (FileNotFoundException)
         {
             // Displays an error message if the file is not found within the containind folder.
             Console.WriteLine($"File not existing.\n");
         }
         // Returns the loaded strings into an array.
-        return _logSummary;
+        return _fileLogSummary;
     }
 
     private string GetFileName()
     {
         Console.WriteLine("What is the filename? ");
-        Activity activity = new Activity();
-        _filename = activity.GetUserInput();
+        _filename = Console.ReadLine();
         return _filename;
+    }
+
+    private List<string> LogEntry()
+    {
+        string date = $"{_currentDate}, {_currentTime}";
+        string breathingLog = $"{_breathingCounter}x Breathing Activity";
+        string reflectingLog= $"{_reflectingCounter}x Reflecting Activity";
+        string listingLog = $"{_listingCounter}x Listing Activity";
+        string line = "You have not done any activities yet.";
+
+        _logSummary.Clear();
+        _logSummary.Add(date);
+
+        if (_breathingCounter != 0 || _reflectingCounter != 0 || _listingCounter != 0)
+        {
+            // Removes the line if the user has performed any of the three activites: Breathing,
+            // Reflecting or Listing according to their counters.
+            _logSummary.Remove(line);
+
+            if (_breathingCounter != 0)
+            {
+                _logSummary.Add(breathingLog);
+            }
+
+            if (_reflectingCounter != 0)
+            {
+                _logSummary.Add(reflectingLog);
+            }
+
+            if (_listingCounter != 0)
+            {
+                _logSummary.Add(listingLog);
+            }
+        }
+
+        else
+        {
+            _logSummary.Add(line);
+        }
+
+        return _logSummary;
     }
 }
