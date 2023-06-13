f635fe7f459b7cdb7a3f223d353b1e4ea95ae569
Camille Rubia <va.camillerubia@gmail.com>
2023-06-13 17:27:21 +0800

    Completed Learning05 Activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -5,5 +5,19 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning05 World!");
+        Console.Clear();
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(new Square("green", 5));
+        shapes.Add(new Rectangle("red", 2, 5));
+        shapes.Add(new Circle("white", 5));
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            Console.WriteLine(color);
+            double area = shape.GetArea();
+            Console.WriteLine(area);
+        }
     }
 }
