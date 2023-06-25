0ae297721a763b41e7e0e4ba1c905bf76fe20a02
Camille Rubia <va.camillerubia@gmail.com>
2023-06-25 23:59:56 +0800

    Created constructor for base class and display goals

--- prove/Develop05/Goal.cs
+++ prove/Develop05/Goal.cs
@@ -4,13 +4,26 @@ using System.Collections.Generic;
 public abstract class Goal
 {
     protected string _goalName;
-    protected string _goalDescription;
+    protected string _description;
     protected int _points;
     protected bool _completed;
+    private string _goalStatus;
 
+    // Initialize the values of each property with specific values 
+    // when a class instantiation is created.
+    public Goal(string goalName, string description, int points)
+    {
+        _goalName = goalName;
+        _description = description;
+        _points = points;
+        _completed = false;
+    }
+
+    // Displays each goal when user chooses List Goal option in the main menu.
     public virtual void DisplayGoal()
     {
-
+        string goalStatus = GoalStatus();
+        Console.WriteLine($"{goalStatus} {_goalName} ({_description})");
     }
 
     public virtual void IsComplete()
@@ -21,6 +34,20 @@ public abstract class Goal
     public int GetPoints()
     {
         return _points;
+    }
+
+    private string GoalStatus()
+    {
+        if (_completed)
+        {
+            _goalStatus = "[X]";
+        }
+        else
+        {
+            _goalStatus = "[ ]";
+        }
+
+        return _goalStatus;
     }
 
 }
