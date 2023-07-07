5b1f32e2e4e7ccc72377dfb97d97173c2a8c13e5
Camille Rubia <va.camillerubia@gmail.com>
2023-07-07 20:15:25 +0800

    Changed generic titles, names and added comments.

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -4,23 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        Console.Clear();
+        Console.WriteLine("Hello Foundation1 World!\n");
 
         List<Video> videos = new List<Video>();
 
         // Create videos and add comments
-        Video video1 = new Video("Video 1", "Author 1", 120);
-        video1.AddComment("User1", "Great video!");
-        video1.AddComment("User2", "Thanks for sharing!");
-        video1.AddComment("User3", "I learned a lot.");
+        Video video1 = new Video("Simplify Your Life with ABC: The Ultimate Solution for Everyday Challenges!", "Garance Zhenya", 120);
+        video1.AddComment("Susanna Liora", "Great video!");
+        video1.AddComment("Ansbert Zéphyrine", "Thanks for sharing!");
+        video1.AddComment("Olusegun Alivia", "I learned a lot.");
 
-        Video video2 = new Video("Video 2", "Author 2", 180);
-        video2.AddComment("User1", "Awesome content!");
-        video2.AddComment("User2", "Keep up the good work!");
+        Video video2 = new Video("Step into the Future: PQR - The Next Generation of Tech Evolution!", "Daley Raimo", 180);
+        video2.AddComment("Ciarán Wynn", "Awesome content!");
+        video2.AddComment("Shivali Caolán", "Keep up the good work!");
 
-        Video video3 = new Video("Video 3", "Author 3", 150);
-        video3.AddComment("User1", "Very informative.");
-        video3.AddComment("User2", "I have a question, can you help?");
+        Video video3 = new Video("Unleash Your Creativity: Introducing XYZ - Inspiring the Artist Within!", "Melita Shukriyya", 150);
+        video3.AddComment("Laurie River", "Very informative.");
+        video3.AddComment("Tommie Nicky", "I have a question, can you help?");
+        video3.AddComment("Lacey Hikari", "Can't wait to try it out!");
+        video3.AddComment("Schuyler Purdie", "This video is eye-opening!");
+        video3.AddComment("Pat Connie", "This video got me excited to try it!");
+
 
         videos.Add(video1);
         videos.Add(video2);    
@@ -29,16 +34,16 @@ class Program
         // Display video information and comments
         foreach (Video video in videos)
         {
-            Console.WriteLine("Title: " + video._trackTitle);
-            Console.WriteLine("Author: " + video._author);
-            Console.WriteLine("Length: " + video._length + " seconds");
-            Console.WriteLine("Number of Comments: " + video.GetCommentCount());
+            Console.WriteLine($"Title: {video._trackTitle}");
+            Console.WriteLine($"Author: {video._author}");
+            Console.WriteLine($"Length: {video._length} seconds");
+            Console.WriteLine($"Number of Comments: {video.GetCommentCount()}");
 
-            Console.WriteLine("Comments:");
+            Console.WriteLine("\nComments:");
             foreach (Comment comment in video.GetComments())
             {
-                Console.WriteLine("Commenter: " + comment._commenterName);
-                Console.WriteLine("Text: " + comment._commentText);
+                Console.WriteLine($"Commenter: {comment._commenterName}");
+                Console.WriteLine($"Text: \"{comment._commentText}\"");
                 Console.WriteLine();
             }
 
